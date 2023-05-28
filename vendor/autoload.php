7d8ccc283073860c0be0cafc47d07e078c357493
Grey Doge <game4fun203@gmail.com>
2023-05-28 13:25:15 +0700

    test fix

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit22fdece4e3b334c8d471e5ebc173c3e6::getLoader();
+return ComposerAutoloaderInitd8ef5eba7c46972f43a7d8f9758c4cd1::getLoader();
