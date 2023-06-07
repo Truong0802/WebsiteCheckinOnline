5b54acc218fa95d2b9a5a77669a2f22b77bf9c5d
Grey Doge <game4fun203@gmail.com>
2023-06-07 21:47:04 +0700

    Thêm view thêm xóa sửa sinh viên

--- app/Http/Controllers/SinhVienController.php
+++ app/Http/Controllers/SinhVienController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SinhVienController extends Controller
 {
     public function admin()
     {
+        return view('admin/student-add');
+    }
+
+    public function themSinhVien()
+    {
+        $sinhVien = DB::table('sinh_vien');
+
+        $sinhVien -> MSSV = $request->input('MSSV');
+        $sinhVien -> HoTenSV = $request->input('HoTenSV');
+        $sinhVien -> TenLop = $request->input('TenLop');
+
+        $sinhVien->save();
+
         return view('admin/student-add');
     }
 }   
--- resources/views/admin/student-add.blade.php
+++ resources/views/admin/student-add.blade.php
@@ -1,5 +1,242 @@
 @extends('layouts.master-admin')
 
 @section('content')
+<div id="ribbon">
+            <span class="ribbon-button-alignment">
+                <span class="btn btn-ribbon" id="refresh" placement="bottom">
+                    <i class="fa fa-refresh"></i>
+                </span>
+            </span>
+            <ol class="breadcrumb">
+                <li class="ng-star-inserted">
+                    <a>Thêm sinh viên</a>
+                </li>
+            </ol>
+        </div>
+        <div class="mt-4" id="content">
+            <div class="  mx-4">
+                <div class="row mb-3">
+                    <div class="col-md-6 pr-0">
+                        <div>
+                            <h1 class="page-title txt-color-blueDark">
+                                <i class="fa-fw fa fa-graduation-cap"></i> Thêm sinh viên
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <form action='/tim-kiem-sinh-vien' method='get'>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="student-name">Họ tên:</label>
+                                <input type="text" class="form-control" id="student-name" name="studentname">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="student-id">MSSV:</label>
+                                <input type="text" class="form-control" id="student-id" name="mssv">
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="class-name">Tên lớp:</label>
+                                <input type="text" class="form-control" id="class-name" name="classname">
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-primary" onclick="filterData()">Tìm kiếm</button>
+                    <a type="button" href="/xoa-tim-kiem-sv" class="btn btn-primary" onclick="removeFilterData()">Xóa tất cả bộ lọc</a>
+                    @csrf
+                </form>
+                <div class="col-md-12 detail">
+                <style>
+                    .detail
+                    {
+                        grid-template-columns: 15fr
+                    }
+                </style>
+                <div class="class-list">
+                    <span><strong>HỌC PHẦN:</strong> Lập trình ứng dụng với Java <strong> (CMP3025) </strong> - Nhóm 2 - Số tín chỉ: 3</span>
+                    <br><br>
+                    <div class="table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>STT</td>
+                                    <td>Mã SV</td>
+                                    <td>Họ tên</td>
+                                    <td>Tên lớp</td>
+                                    <td></td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>20111061224</td>
+                                    <td>Trần Nguyên Trường</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>2</td>
+                                    <td>2011060957</td>
+                                    <td>Hồ Phú Tài</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>3</td>
+                                    <td>2011062236</td>
+                                    <td>Nguyễn Phương Minh</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>4</td>
+                                    <td>20111061224</td>
+                                    <td>Trần Nguyên Trường</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>5</td>
+                                    <td>2011060957</td>
+                                    <td>Hồ Phú Tài</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                        
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>6</td>
+                                    <td>2011062236</td>
+                                    <td>Nguyễn Phương Minh</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>7</td>
+                                    <td>20111061224</td>
+                                    <td>Trần Nguyên Trường</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>8</td>
+                                    <td>2011060957</td>
+                                    <td>Hồ Phú Tài</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                        
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>9</td>
+                                    <td>2011062236</td>
+                                    <td>Nguyễn Phương Minh</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>10</td>
+                                    <td>20111061224</td>
+                                    <td>Trần Nguyên Trường</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>11</td>
+                                    <td>2011060957</td>
+                                    <td>Hồ Phú Tài</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                        
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>12</td>
+                                    <td>2011062236</td>
+                                    <td>Nguyễn Phương Minh</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>13</td>
+                                    <td>20111061224</td>
+                                    <td>Trần Nguyên Trường</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>14</td>
+                                    <td>2011060957</td>
+                                    <td>Hồ Phú Tài</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                        
+                                </tr>
+                            </tbody>
+                            <tbody>
+                                <tr>
+                                    <td>15</td>
+                                    <td>2011062236</td>
+                                    <td>Nguyễn Phương Minh</td>
+                                    <td>20DTHA2</td>
+                                    <td>Chỉnh sửa</td>
+                                    <td>Xóa sinh viên</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+</div>
 
 @stop   
