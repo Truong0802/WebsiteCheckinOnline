235e25d0ec32290c79ba62144b8196267a408bcb
Grey Doge <game4fun203@gmail.com>
2023-06-10 21:59:05 +0700

    Fix layout admin danh sách lớp

--- routes/web.php
+++ routes/web.php
@@ -25,28 +25,38 @@ use App\Http\Controllers\SinhVienController;
 //         return view('Account.login');
 //  });
 Route::get('/',[AccountController::class,'index']);
+
 //Đăng nhập
 Route::post('/account-login',[AccountController::class,'login']);
+
 //Đăng xuất
 Route::get('/logout',[AccountController::class,'logout']);
+
 //Trang thời khóa biểu sinh viên
 Route::get('/trang-chu',[HomeController::class,'trangchusv']);
+
 //Trang danh sách lớp của giảng viên
 Route::get('/danh-sach-lop',[TeacherController::class,'danhsachlop']);
+
 //Danh sách sinh viên
 Route::get('/danh-sach-sinh-vien',[TeacherController::class,'danhsachsinhvien']);
+
 //Tìm kiếm trong danh sách lớp
 Route::get('/tim-kiem',[TeacherController::class,'timkiem']);
+
 //
 Route::get('/xoa-tim-kiem',[TeacherController::class,'removetimkiem']);
 
 //Tìm kiếm trong danh sách sinh viên
 Route::get('/tim-kiem-sinh-vien',[TeacherController::class,'timkiemsinhvien']);
+
 //
 Route::get('/xoa-tim-kiem-sv',[TeacherController::class,'removetimkiemsv']);
+
 //Điểm danh
 Route::get('/diem-danh',[TeacherController::class,'DiemDanh'])->name('diemdanh');
 Route::get('/form-diem-danh',[TeacherController::class,'qrcodeGenerate']);
+
 //
 Route::get('/tro-ve',[TeacherController::class,'trovedanhsach']);
 //Hỗ trợ
